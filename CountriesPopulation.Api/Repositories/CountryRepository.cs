3a38f04ab4acc8a553f657f84fd2e09a90f2619e
Mohamed Husein <mhussein399@gmail.com>
2022-12-19 22:40:25 +0200

    implement repository and irepository

--- CountriesPopulation.Api/Repositories/CountryRepository.cs
+++ CountriesPopulation.Api/Repositories/CountryRepository.cs
@@ -1,10 +1,74 @@
 ﻿using System;
+using CountriesPopulation.Api.Dtos;
+using CountriesPopulation.Api.EfCore;
+using CountriesPopulation.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace CountriesPopulation.Api.Repositories
 {
-    public class CountryRepository
+    public class CountryRepository : ICountryRepository<Country>
     {
-        public CountryRepository()
+        private readonly CountriesPopulationDbContext _dbContext;
+        public CountryRepository(CountriesPopulationDbContext dbContext) 
         {
+            _dbContext = dbContext;
+        }
+        public async void  AddMany(List<Country> data)
+        {
+            await _dbContext.Set<Country>().AddRangeAsync(data);
+            _dbContext.SaveChanges();
+        }
+        public void Add(Country data)
+        {
+            _dbContext.Set<Country>().Add(data);
+            _dbContext.SaveChanges();
+        }
+        public async Task Update(Country data)
+        {
+            var country = await _dbContext.Countries.Include(c=>c.PopulationCount).FirstOrDefaultAsync(c => c.Name == data.Name);
+            if(country == null)
+                return;
+            country.Name = data.Name;
+            country.Code = data.Code;
+            country.Iso3 = data.Iso3;
+            UpdatePopulation(country, data.PopulationCount);
+            _dbContext.SaveChanges();
+        }
+        private void UpdatePopulation(Country country, List<Population> population)
+        {
+            var countryPopulation = country.PopulationCount;
+            if(population == null)
+                countryPopulation = new List<Population>();
+            if(countryPopulation != null){
+                foreach(var pop in countryPopulation)
+            {
+                _dbContext.Populations.Remove(pop);
+            }
+            }
+            
+            foreach(var pop in population)
+            {
+                pop.CountryId = country.Id;
+                _dbContext.Populations.Add(pop);
+            }
+            _dbContext.SaveChanges();
+
+        }
+        public async Task<List<Country>> GetPagedList(int page)
+        {
+            return  await ( _dbContext.Countries.Include(c =>c.PopulationCount).AsNoTracking().Skip((page - 1) * 50).Take(50).ToListAsync());
+        }
+        public async Task<List<Country>> GetList()
+        {
+            return  await ( _dbContext.Countries.Include(c =>c.PopulationCount).AsNoTracking().ToListAsync());
+        }
+        public async Task<Country> Get(Guid id)
+        {
+            return await _dbContext.Countries.Include(c => c.PopulationCount).FirstOrDefaultAsync(c => c.Id == id);
+        }
+        public async Task<Country> Get(string search)
+        {
+            return await _dbContext.Countries.Include(c => c.PopulationCount).FirstOrDefaultAsync(c => c.Name == search || c.Code == search);
         }
     }
 }
---
+++ CountriesPopulation.Api/Repositories/ICountryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace CountriesPopulation.Api.Repositories
+{
+    public interface ICountryRepository<TEntity>
+    {
+        void AddMany(List<TEntity> entity);
+        void Add(TEntity entity);
+        Task Update(TEntity entity);
+        Task<List<TEntity>> GetList();
+        Task<List<TEntity>> GetPagedList(int page);
+        Task<TEntity> Get(Guid id);
+        Task<TEntity> Get(string search);
+    }
+}
+
