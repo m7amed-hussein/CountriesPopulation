55f46b1c7a36ac66304619757a62a3b50fd45cb7
Mohamed Husein <mhussein399@gmail.com>
2022-12-19 23:54:30 +0200

    refactor country controller

--- CountriesPopulation.Api/Controllers/CountryController.cs
+++ CountriesPopulation.Api/Controllers/CountryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CountriesPopulation.Api.Dtos;
 using CountriesPopulation.Api.Models;
+using CountriesPopulation.Api.Services;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -13,13 +14,69 @@ namespace CountriesPopulation.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CountryController : Controller
+    public class CountryController : ControllerBase
     {
-            private static readonly HttpClient client = new HttpClient();
+            private readonly IHttpClientFactory _clientFactory;
+            private readonly ICountryService _service;
 
-            [HttpGet]
-            public async Task<ActionResult<ApiResponse>> GetData()
+            public CountryController(ICountryService service , IHttpClientFactory clientFactory)
             {
+                _service = service;
+                _clientFactory = clientFactory;
+            }
+            //Get country
+            [HttpGet]
+            [Route("Get")]
+            public async Task<ActionResult<Country>> GetCountry(Guid id)
+            {
+                try
+                {
+                    var country = await _service.Get(id);
+                    if(country == null)
+                        return NotFound(new { error = "Country not found" });
+                    return Ok(country);
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(500, new { error = ex.Message });
+                }
+            }
+            [HttpGet]
+            [Route("GetPaged")]
+            public async Task<ActionResult<List<Country>>> GetCountries(int page)
+            {
+                try
+                {
+                    var countries = await _service.GetList(page);
+                    return Ok(countries);
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(500, new { error = ex.Message });
+                }
+            }
+            //Get country by name or code
+            [HttpGet]
+            [Route("GetByNameOrCode")]
+            public async Task<ActionResult<Country>> GetCountry(string search)
+            {
+                try
+                {
+                    var country = await _service.Get(search);
+                    if(country == null)
+                        return NotFound(new { error = "Country not found" });
+                    return Ok(country);
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(500, new { error = ex.Message });
+                }
+            }
+            [HttpPost]
+            [Route("Sync")]
+            public async Task<ActionResult> SyncCountries()
+            {
+                var client = _clientFactory.CreateClient();
                 try
                 {
                     // Make an HTTP GET request to the API
@@ -29,12 +86,11 @@ namespace CountriesPopulation.Api.Controllers
                     var content = await response.Content.ReadAsStringAsync();
                     var apiResponse = JsonConvert.DeserializeObject<ApiResponse>(content);
 
-                    // Return the data to the client
-                    return Ok(apiResponse.Data);
+                    await _service.SyncFromApi(apiResponse.Data);
+                    return Ok();
                 }
                 catch (Exception ex)
                 {
-                    // Return an error response if there was a problem calling the API
                     return StatusCode(500, new { error = ex.Message });
                 }
             }
