7b11fb2c263dddb3d0d4bdcd5ff350c81beda793
Mohamed Husein <mhussein399@gmail.com>
2022-12-19 22:34:30 +0200

    update value property

---
+++ CountriesPopulation.Api/Migrations/20221219191448_change value to int 64.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CountriesPopulation.Api.Migrations
+{
+    public partial class changevaluetoint64 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Count",
+                table: "Populations",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Count",
+                table: "Populations",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
